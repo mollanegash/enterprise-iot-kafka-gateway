d661a8df403979d1b19a0dda6bf35a5399b65870
mollanegash <mdraya3@gmail.com>
2026-07-24 11:49:19 -0500

    Initial Spring Boot setup

---
+++ src/main/java/com/example/enterprise_iot_kafka_gateway/EnterpriseIotKafkaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterprise_iot_kafka_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseIotKafkaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseIotKafkaGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/enterprise_iot_kafka_gateway/EnterpriseIotKafkaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterprise_iot_kafka_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseIotKafkaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
